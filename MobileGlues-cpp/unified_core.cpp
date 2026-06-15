588828d9cc1f79a126684bf12b13f135537df080
denotas235 <recebeumoneyantonio@gmail.com>
2026-06-16 00:53:54 +0200

    feat: MobileGlues Reborn Phase 5 (Unified Core, Native ASTC Loader, V-Sight Culling)

---
+++ MobileGlues-cpp/unified_core.cpp
@@ -0,0 +1,32 @@
+#include "unified_core.h"
+
+// Instâncias globais do Motor Unificado
+MobileGlues::NativeChunkManager g_ChunkManager;
+
+extern "C" {
+
+// 1. JNI para o V-Sight Culling
+JNIEXPORT jboolean JNICALL Java_com_nexus_astcmod_UnifiedCoreJNI_isEntityInFocus(
+    JNIEnv* env, jclass clazz, 
+    jfloat px, jfloat py, jfloat pz, 
+    jfloat lx, jfloat ly, jfloat lz, 
+    jfloat ex, jfloat ey, jfloat ez) 
+{
+    return MobileGlues::VSightController::isEntityInFocus(px, py, pz, lx, ly, lz, ex, ey, ez);
+}
+
+// 2. JNI para Memória Nativa (Chunks)
+JNIEXPORT void JNICALL Java_com_nexus_astcmod_UnifiedCoreJNI_allocateChunk(JNIEnv* env, jclass clazz, jint x, jint z) {
+    g_ChunkManager.allocateChunk(x, z);
+}
+
+JNIEXPORT void JNICALL Java_com_nexus_astcmod_UnifiedCoreJNI_freeChunk(JNIEnv* env, jclass clazz, jint x, jint z) {
+    g_ChunkManager.freeChunk(x, z);
+}
+
+// 3. JNI para Luz Paralela
+JNIEXPORT void JNICALL Java_com_nexus_astcmod_UnifiedCoreJNI_propagateLightAsync(JNIEnv* env, jclass clazz, jint chunkX, jint chunkZ) {
+    MobileGlues::StarlightNative::propagateLightAsync(chunkX, chunkZ);
+}
+
+}
