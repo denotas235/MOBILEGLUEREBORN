047377e82c1eac186f2a950cc532a12467d3e888
denotas235 <recebeumoneyantonio@gmail.com>
2026-06-17 23:00:26 +0200

    feat: expose fixShaderForMali via JNI

--- MobileGlues-cpp/unified_core.cpp
+++ MobileGlues-cpp/unified_core.cpp
@@ -30,5 +30,33 @@ JNIEXPORT void JNICALL Java_com_nexus_astcmod_UnifiedCoreJNI_propagateLightAsync
     MobileGlues::StarlightNative::propagateLightAsync(chunkX, chunkZ);
 }
 
+// 4. JNI para Fix de Shaders
+JNIEXPORT jstring JNICALL Java_com_nexus_astcmod_UnifiedCoreJNI_fixShaderForMali(JNIEnv* env, jclass clazz, jstring shaderSource) {
+    const char* nativeString = env->GetStringUTFChars(shaderSource, 0);
+    std::string source(nativeString);
+    
+    // 1. Injeta precisao
+    if (source.find("precision ") == std::string::npos) {
+        size_t versionPos = source.find("#version");
+        if (versionPos != std::string::npos) {
+            size_t insertPos = source.find('\n', versionPos);
+            if (insertPos != std::string::npos) {
+                source.insert(insertPos + 1, "precision highp float;\nprecision highp int;\n");
+            }
+        } else {
+            source.insert(0, "precision highp float;\nprecision highp int;\n");
+        }
+    }
+    
+    // 2. Transpila
+    size_t pos = 0;
+    while((pos = source.find("varying ", pos)) != std::string::npos) { source.replace(pos, 8, "in "); pos += 3; }
+    pos = 0;
+    while((pos = source.find("attribute ", pos)) != std::string::npos) { source.replace(pos, 10, "in "); pos += 3; }
+
+    env->ReleaseStringUTFChars(shaderSource, nativeString);
+    return env->NewStringUTF(source.c_str());
+}
+
 }
 #endif
