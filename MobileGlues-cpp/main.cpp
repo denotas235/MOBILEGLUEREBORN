20adaa0518757fdfc28d3afd93f827f7d4e1916d
denotas235 <recebeumoneyantonio@gmail.com>
2026-06-20 00:38:26 +0200

    fix: restore effects module initialization in main.cpp
    Restore calls to effects modules (extension_scanner, phase2_lighting, 
    mali_sorter, shader_binary_cache) while NOT including shader_sanitizer 
    and shader_patcher which were removed. This keeps all game effects working 
    while fixing shader core compilation issues.

--- MobileGlues-cpp/main.cpp
+++ MobileGlues-cpp/main.cpp
@@ -14,6 +14,11 @@
 #include "gl/mg.h"
 #include "gles/loader.h"
 #include "includes.h"
+#include "gl/extension_scanner.h"
+#include "gl/phase2_lighting.h"
+#include "gl/mali_sorter.h"
+#include "gl/shader_binary_cache.h"
+
 #include <cerrno>
 #include <cstring>
 #include <sys/stat.h>
@@ -62,6 +67,10 @@ void proc_init() {
     load_libs();
     init_target_egl();
     init_target_gles();
+    init_extension_scanner();
+    phase2_init();
+    mali_sorter_init();
+    shader_binary_cache_init();
     set_multidraw_setting();
 
     init_settings_post();
